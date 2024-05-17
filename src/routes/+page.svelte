94a29b6eafc31ca78fef6ddeb3f904a985e3ff03
Joseph <108951296+Josephfallen@users.noreply.github.com>
2024-05-17 18:31:36 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,9 @@
     import { writable } from "svelte/store";
     import { goto } from "$app/navigation";
     import { debounce } from "$lib/debounce";
-    const userPlaceholders = [
-        "natix",
-        "Frosty",
-        "Pan",
-        "Sandie"
-    ];
-
+    import './styles/global.css';
+    
+    const userPlaceholders = ["natix", "Frosty", "Pan", "Sandie"];
     const hoursSinceEpoch = Math.floor((new Date()).getTime() / 1000 / 60 / 60);
     const userPlaceholder = userPlaceholders[hoursSinceEpoch % userPlaceholders.length];
 
@@ -23,51 +19,30 @@
         if (searchQuery.trim() === "") {
             $searchResults = [];
         } else {
-            $searchResults = await apiClient.searchForUserByName(searchQuery);
+            searchResults.set(await apiClient.searchForUserByName(searchQuery));
         }
     }
 </script>
 
-<style>
-    .users {
-        display: flex;
-        flex-direction: column;
-        gap: .5rem;
-        margin: .5rem;
-    }
-    .user {
-        width: 10rem;
-        height: 2rem;
-        border: 1px solid #000;
+<title>VAIL stats far from this world</title>
+<meta name="description" content="View official vail stats for any vail user">
+<link rel="stylesheet" href="global.css">
 
-        background: inherit;
-        text-align: left;
-    }
-    .user:hover {
-        color: #555;
-    }
+<div class="container">
+    <h1>Vail stats</h1>
+    <p>Delivered to you from far outside this world.</p>
 
-</style>
+    <h1>Search for user</h1>
+    <form on:submit|preventDefault={() => {}}>
+        <label for="username-field">Username</label>
+        <input id="username-field" type="search" placeholder={userPlaceholder} on:input={debounce(onSearchFieldChange)} bind:value={searchQuery}>
+    </form>
 
-<svelte:head>
-    <title>Vail stats far from this world</title>
-    <meta name="description" content="View official vail stats for any vail user">
-</svelte:head>
-
-<h1>Vail stats</h1>
-<p>Delivered to you from far outside this world.</p>
-
-<h1>Search for user</h1>
-<form on:submit|preventDefault={() => {}}>
-    <label for="username-field">Username</label>
-    <br/>
-    <input id="username-field" type="search" placeholder={userPlaceholder} on:input={debounce(onSearchFieldChange)} bind:value={searchQuery}>
-</form>
-
-<div class="users">
-    {#each $searchResults as user}
-        <button class="user" on:click={() => {goto(`/players/${user.id}`)}}>
-            {user.name}
-        </button>
-    {/each}
+    <div class="users">
+        {#each $searchResults as user}
+            <button class="user" on:click={() => {goto(`/players/${user.id}`)}}>
+                {user.name}
+            </button>
+        {/each}
+    </div>
 </div>
