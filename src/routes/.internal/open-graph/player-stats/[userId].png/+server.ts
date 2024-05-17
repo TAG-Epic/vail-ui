b3d3e6609893dcb02bf1666cc6ab6d65f3dac084
Joseph <108951296+Josephfallen@users.noreply.github.com>
2024-05-17 18:04:12 -0400

    Create +server.ts

---
+++ src/routes/.internal/open-graph/player-stats/[userId].png/+server.ts
@@ -0,0 +1,44 @@
+import ShotVisualizer from "$lib/elements/ShotVisualizer.svelte";
+import type { PageLoad } from "./$types.d.ts";
+import { APIClient } from "$lib/api";
+import type { UserGunStats } from "$lib/api";
+import { createCombinationVirtualGun } from "$lib/virtual_gun";
+import { error } from '@sveltejs/kit';
+import { writable } from "svelte/store";
+import { Resvg }  from "@resvg/resvg-js";
+
+export async function GET(request: PageLoad) {
+    const userId = request.params.userId
+
+    const apiClient = new APIClient(request.fetch);
+
+    let svg: string;
+    
+    try {
+        const userStats = await apiClient.getUserStats(userId);
+        const guns = Object.values(userStats.weapons).filter(weapon => weapon.shots !== undefined);
+        const gun = createCombinationVirtualGun(guns);
+        const gunStore = writable<UserGunStats>(gun);
+        const renderResult = ShotVisualizer.render({svg: true, stats: gunStore});
+        svg = renderResult.html;
+    } catch (errorDetails) {
+        if (errorDetails.code === undefined || errorDetails.detail === undefined) {
+            throw errorDetails;
+        }
+        if (errorDetails.code === "user_not_found") {
+            return error(404, {
+                "message": "User not found"
+            });
+        }
+        throw errorDetails;
+    }
+
+    const resvg = new Resvg(svg, {});
+    const pngData = resvg.render().asPng();
+    return new Response(pngData, {
+        headers: {
+            "Content-Type": "image/png"
+        }
+    });
+    
+}
